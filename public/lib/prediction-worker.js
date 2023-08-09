57dac8f8537711369ac0e1daad61f8f091fff334
Simon Chen <presenter.simon@gmail.com>
2023-08-09 12:03:25 -0700

    mostlikely as prediction result, abandon predictions array

--- public/lib/prediction-worker.js
+++ public/lib/prediction-worker.js
@@ -6,7 +6,7 @@ self.addEventListener('message', async event => {
   const formData = new FormData();
   formData.append('image', imageBlob, 'image.png'); // Adjust filename and type as needed
   console.log('formD ', formData)
-  console.log('predKey ', this.predictionKey)
+  console.log('predKey ', predictionKey)
   const response = await fetch(predictionEndpoint, {
     method: 'POST',
     body: formData,
@@ -15,7 +15,21 @@ self.addEventListener('message', async event => {
     },
   });
 
-  const result = await response.json();
-  console.log('web worker result ', result);
+  let result = await response.json();
+  const mostLikelyPrediction = result.predictions
+    .sort((a, b) => b.probability - a.probability)
+    .slice(0, 1)[0];
+
+  if (mostLikelyPrediction) {
+    // Prepare the showTag object with the prediction details and image source
+    const mostLikely = {
+      tag: mostLikelyPrediction.tagName,
+      probability: Math.floor(mostLikelyPrediction.probability * 100),
+    //  image: imageDataSource, // Add the image source to the showTag output
+      boundingBox: mostLikelyPrediction.boundingBox
+    };
+    console.log('worker-mostlikely ', mostLikely)
+    result = mostLikely
+  }
   self.postMessage(result);
 });
