1f76eb730cc560ea7330b97190fb0c3a09f31453
Simon Chen <presenter.simon@gmail.com>
2023-08-26 10:21:11 -0700

    Improve readability: checkWorker, uploadWorker

--- public/lib/check-worker.js
+++ public/lib/check-worker.js
@@ -1,39 +1,38 @@
 self.addEventListener('message', async event => {
-  const imageBlob = event.data.imageBlob;
-  const card = event.data.card
+  const { imageBlob, card } = event.data;
+  const { keyContain, endConnect, color, probability } = card;
 
-    const predictionKey = card.keyContain;
-    const predictionEndpoint = card.endConnect;
-    const color = card.color;
-    const threshold = card.probability;
-    const formData = new FormData();
-    formData.append('image', imageBlob, 'image.png'); // Adjust filename and type as needed
+  const formData = new FormData();
+  formData.append('image', imageBlob, 'image.png');
 
-    const checkResponse = await fetch(predictionEndpoint, {
-      method: 'POST',
-      body: formData,
-      headers: {
-        'Prediction-Key': predictionKey,
-      },
-    });
-    const result = await checkResponse.json();
-    if (result.predictions && Array.isArray(result.predictions)) {
-      // Filter and sort predictions
-      const sortedPredictions = result.predictions
-        .filter(prediction => prediction.probability > threshold)
-        .sort((a, b) => b.probability - a.probability);
-      const mostLikely = sortedPredictions[0];
-    if (mostLikely) {
-      // Prepare the response object with the prediction details and image source
-      const checkResponse = {
+  const predictionResponse = await fetch(endConnect, {
+    method: 'POST',
+    body: formData,
+    headers: {
+      'Prediction-Key': keyContain,
+    },
+  });
+
+  const predictionResult = await predictionResponse.json();
+  if (predictionResult.predictions && Array.isArray(predictionResult.predictions)) {
+    const sortedPredictions = predictionResult.predictions
+      .filter(prediction => prediction.probability > probability)
+      .sort((a, b) => b.probability - a.probability);
+
+    const mostLikelyPrediction = sortedPredictions[0];
+    
+    if (mostLikelyPrediction) {
+      const responsePayload = {
         initTime: new Date().getTime(),
         imageBlob: imageBlob,
-        tag: mostLikely.tagName,
+        tag: mostLikelyPrediction.tagName,
         color: color,
-        probability: Math.floor(mostLikely.probability * 100),
-        boundingBox: mostLikely.boundingBox
-      } 
-      self.postMessage(checkResponse);
+        probability: Math.floor(mostLikelyPrediction.probability * 100),
+        boundingBox: mostLikelyPrediction.boundingBox,
+      };
+
+      self.postMessage(responsePayload);
     }
   }
-  });
+});
+
--- public/lib/upload-worker.js
+++ public/lib/upload-worker.js
@@ -1,22 +1,15 @@
 self.addEventListener('message', async event => {
-  const imageBlob = event.data.imageBlob;
-  const card = event.data.card;
-  const containerName = card.keyContain;
-  const connectionString = card.endConnect;
-  const color = card.color;
-  const threshold = card.probability;
+  const { imageBlob, card } = event.data;
+  const { keyContain: containerName, endConnect: connectionString, color, probability } = card;
 
-  // Use Date object to format the date in YYYYMMDD format
   const now = new Date().toISOString().slice(0, 10).replace(/-/g, '').slice(0, 14);
-
-  const fileName = card.id + now + '.png';
+  const fileName = `${card.id}${now}.png`;
   
   const formData = new FormData();
-
-  // Append imageBlob to formData
   formData.append('imageFile', imageBlob, fileName);
 
-  console.log('formData loaded'); // This will immediately log after appending
+  console.log('formData loaded');
+
   try {
     if (formData) {
       const response = await fetch('/saveblob', {
@@ -25,17 +18,17 @@ self.addEventListener('message', async event => {
       });
 
       const result = await response.json();
-      console.log('saveBlob result**', result);
+      console.log('saveBlob result:', result);
 
       const checkResponse = {
         initTime: new Date().getTime(),
         imageBlob: imageBlob,
         tag: card.id,
         pendingCount: 1,
-        color:card.color,
-        probability: card.probability,
-        boundingBox: { top: 0, left: 0, width: 1, height: 1,}
-      } 
+        color: color,
+        probability: probability,
+        boundingBox: { top: 0, left: 0, width: 1, height: 1 },
+      };
 
       self.postMessage(checkResponse);
     }
@@ -43,6 +36,5 @@ self.addEventListener('message', async event => {
     console.error('Error:', error);
   }
 });
-
 
 
